a6e90dfb181fbdb03364189eede7f755c35ccf32
Ariel Semik <semikariel@gmail.com>
2019-04-18 15:08:26 +0200

    Wprowadzona obsluga rejestracji nowego klienta

--- Python/Core.py
+++ Python/Core.py
@@ -13,28 +13,8 @@ class DBConect:
         wyniki = self.kursor.fetchall()
         print(wyniki)
 
-class User:
-    def __init__(self, name, surname, email, password):
-        self.name = name
-        self.surname = surname
-        self.email = email
-        self.password = password
 
 
-class Seller(User):
-    def __init__(self, name, surname, email, password, role_name = 'Seller'):
-        super().__init__(name, surname, email, password)
-        self.role_name = role_name
-
-class Customer(User):
-    def __init__(self, name, surname, email, password, role_name = 'Customer'):
-        super().__init__(name, surname, email, password)
-        self.role_name = role_name
-
-class Admin(User):
-    def __init__(self, name, surname, email, password, role_name = 'Admin'):
-        super().__init__(name, surname, email, password)
-        self.role_name = role_name
 class MenuGlowne():
     def strona_startowa(self):
         print("Witaj użytkowniku Sklepu internetowego NAJLEPSZY SKLEP INTERNETOWY śWIATA")
@@ -53,11 +33,53 @@ class MenuGlowne():
         print("Dziękujemy że odwiedzileś naszą witrynę.")
 
     else:
-        print("Nie rozumiem Twojej odpowiedzi.")
+        print("Nie rozumiem Twojej odpowiedzi. Zacznijmy jeszcze raz")
+        strona_startowa()
 
 
 class PanelKlienta():
-    def rejestracja_nowego_klienta(self):
+    def rejestracja_nowego_klienta(self, DBConect, MenuGlowne):
+        print("Witaj na stronie logowania \n \n")
+
+        imie = input('Podaj swoje imie: \n')
+        nazwisko = input("Podaj swoje naziwsko: \n")
+        email = input("Teraz podaj email: \n")
+        haslo = input("wprowadź hasło: \n")
+        haslo2 = input("Powtówrz hasło: \n")
+        while haslo != haslo2:
+            haslo = input("Hasła różnią się od siebie. Wprowadź hasło ponownie: \n")
+            haslo2 = input("Powtówrz hasło: \n")
+
+        firma = input('Wpisz Tak jeżeli rejestrujesz się jako firma? Nie - jeżeli osoba fizyczna \n').strip().upper()
+        if  firma == 'TAK':
+            nip = int(input("Wprowadz nip firmy: \n"))
+        else:
+            nip = 'null'
+
+        adres = input('Wpisz Tak jeżeli chcesz wprowadziić teraz dane adresowe? Nie - jeżeli chcesz je wprowadzić później: \n').strip().upper()
+        if  adres == 'TAK':
+            ulica = input("Wprowadz ulicę i numer: \n")
+            miasto =input('Wprowadż nazwę miasta: \n')
+            kod_pocztowy = input("Wprowadź kod pocztowy: \n")
+        else:
+            ulica = 'null'
+            miasto = 'null'
+            kod_pocztowy ='null'
+
+        DBConect.kursor.execute("INSERT INTO `sklep_internetowy`.`users`(`id`,`name_u`,`surname_u`,`email`,`tax_number`,`role_name`,`street`,`city`,`postal_code`,`pass`,`create_date`) VALUES( null, %s ,%s,%s>,%s>, 'Customer' ,%s,%s,%s>,%s, null)" %(imie,nazwisko, email,nip, ulica, miasto, kod_pocztowy, haslo))
+        decyzja = input("Wpisz TAK jeżeli potwierdzasz załozenie konta w sklepie internetowym NAJLEPSZY SKLEP INTERNETOWY śWIATA\n Lub wpisz Nie jeżelichcesz wrócić do menu głownego \n").strip().upper()
+        while decyzja != 'TAK' or decyzja=='NIE':
+            DBConect.conn.rollback()
+            print("Nie rozumiem odpowiedzi")
+            decyzja = input("Wpisz TAK jeżeli potwierdzasz załozenie konta w sklepie internetowym NAJLEPSZY SKLEP INTERNETOWY śWIATA\n Lub wpisz Nie jeżelichcesz wrócić do menu głownego \n").strip().upper()
+
+        if decyzja == "TAK":
+            DBConect.conn.commit()
+            print("Witaj w naszym sklepie")
+            MenuGlowne.strona_startowa()
+        elif decyzja == "NIE":
+            DBConect.conn.rollback()
+            MenuGlowne.strona_startowa()
 
 
     def aktualizacja_danych_klienta(self):
@@ -81,4 +103,30 @@ class PanelSprzedawcy():
     def dodawanie_produktow_na_stan(self):
 
     def realizacja_zamowienia
+
+
+        """
+        class User:
+            def __init__(self, name, surname, email, password):
+                self.name = name
+                self.surname = surname
+                self.email = email
+                self.password = password
+
+
+        class Seller(User):
+            def __init__(self, name, surname, email, password, role_name = 'Seller'):
+                super().__init__(name, surname, email, password)
+                self.role_name = role_name
+
+        class Customer(User):
+            def __init__(self, name, surname, email, password, role_name = 'Customer'):
+                super().__init__(name, surname, email, password)
+                self.role_name = role_name
+
+        class Admin(User):
+            def __init__(self, name, surname, email, password, role_name = 'Admin'):
+                super().__init__(name, surname, email, password)
+                self.role_name = role_name
+        """
 sprawdzeniepolaczenia = DBConect()
